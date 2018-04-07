2b1774620da49bc6e52e49062c3094d3a41d75c4
Jhonatan <mollitiamteodoro@gmail.com>
2018-04-07 19:15:56 -0300

    Ajuste Admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from .models import User
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .forms import UserAdminCreationForm
+from .forms import UserAdminForm
 
-# Register your models here.
+class UserAdmin(BaseUserAdmin):
+
+    add_form =  UserAdminCreationForm
+    fieldsets = (
+        (None, {
+            'fields': ('username', 'email', 'password1', 'password2')
+        }),
+        ('Informações Básicas', {
+            'fields': ('name', 'last_login' )
+        }),
+        ('Permissões', {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups',
+                       'user_permissions')
+        }),
+    )
+    form = UserAdminForm
+    add_fieldsets = (
+        (None, {
+            'fields': ('username', 'email', 'password1', 'password2')
+                }),
+    )
+    list_display = ['username', 'name', 'email',
+                    'is_active', 'is_staff', 'date_joined']
+
+
+admin.site.register(User, UserAdmin)
--- catalog/apps.py
+++ catalog/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 
 class CatalogConfig(AppConfig):
     name = 'catalog'
+    verbose_name = 'Catálogo'
